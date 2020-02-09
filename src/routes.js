9aeed24dc8abb7e6abd4521f8160175a62ef043d
clebercar <contatoclebercarvalho@gmail.com>
2020-02-09 00:23:31 -0300

    Criar verificação de token

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionsController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionsController.store);
 
+routes.use(authMiddleware);
 export default routes;
