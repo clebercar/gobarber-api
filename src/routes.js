e962349e5f93cc3458a571b7eae32f6eb397c896
clebercar <contatoclebercarvalho@gmail.com>
2020-02-08 23:53:03 -0300

    Criar rota de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionsController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionsController.store);
 
 export default routes;
