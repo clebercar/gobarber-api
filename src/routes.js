cf7736198951d05f03b91ff98e50d6e3241b001f
clebercar <contatoclebercarvalho@gmail.com>
2020-02-10 22:10:23 -0300

    Adicionar configuração do multer ao arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionsController';
@@ -6,6 +8,7 @@ import SessionsController from './app/controllers/SessionsController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionsController.store);
