88ee265428ea34f9b7ed8ae79cb9da7dc62c46a4
clebercar <contatoclebercarvalho@gmail.com>
2020-03-22 00:07:47 -0300

    Criar recurso para listar usuários que são provedores

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionsController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 import AppointmentsController from './app/controllers/AppointmentsController';
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionsController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
